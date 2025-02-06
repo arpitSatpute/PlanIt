c482abe5e34fb7a6e0e906484b05178a062aa504
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-02-06 11:25:53 +0530

    Initial Commit for Backend

--- src/main/java/com/teamarc/olanit/entity/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.teamarc.olanit.entity.enums;
-
-public enum Role {
-    User, ADMIN
-}
---
+++ src/main/java/com/teamarc/planit/services/UserService.java
@@ -0,0 +1,33 @@
+package com.teamarc.planit.services;
+
+
+
+import com.teamarc.planit.entity.User;
+import com.teamarc.planit.entity.enums.Role;
+import com.teamarc.planit.exceptions.ResourceNotFoundException;
+import com.teamarc.planit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElse(null);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+    }
+
+    public User loadUserByRole(Role role) {
+        return userRepository.findByRoles(role);
+    }
+}
